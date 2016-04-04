476abbb6d485f69180eeac986d135d22739aa8ec
Derek Wood <ddwood@outlook.com>
2016-04-03 21:37:11 -0700

    Add functionality to search teams' upcoming schedule

--- plugins/nhl.py
+++ plugins/nhl.py
@@ -1,50 +1,61 @@
 import parsedatetime as pdt
 from dateutil.parser import parse
 from pytz import timezone
+from datetime import timedelta
 
 from util import hook, http
+from nhlutil import NHL_TEAMS, get_date, get_nhl_json, get_scores
 
 EASTERN = timezone('US/Eastern')
 
-def get_scores(inp, status):
-    """Return NHL games with a specific status"""
-    nhl_scores_url = "https://statsapi.web.nhl.com/api/v1/schedule?startDate=%s&endDate=%s&expand=schedule.linescore,schedule.teams"
-    j = get_nhl_json(inp, nhl_scores_url)
-    scores = []
-    games = j['dates'][0]['games']
-    for game in games:
-        if game['status']['abstractGameState'] == status:
-            scores.append(game)
-    return scores
-
-def get_date(inp):
-    """Parse the input as a date and return a format suitable for the nhl.com API"""
-    if not inp:
-        inp = 'today'
-    cal = pdt.Calendar()
-    return cal.parseDT(inp)[0].date().isoformat()
-
-def get_nhl_json(inp, url):
-    """Take the input as a raw date and return json from provided (nhl.com) url"""
-    date = get_date(inp)
-    return http.get_json(url % (date, date))
-
 @hook.command('schedule', autohelp=False)
 def nhl_schedule(inp):
-    """Return the schedule for either the current or specified day"""
-    nhl_schedule_url = "https://statsapi.web.nhl.com/api/v1/schedule?startDate=%s&endDate=%s&expand=schedule.teams,schedule.broadcasts.all"
-    j = get_nhl_json(inp, nhl_schedule_url)
-    schedule = []
-    for game in j['dates'][0]['games']:
-        game_date = parse(game['gameDate']).astimezone(EASTERN).strftime('%-I:%M %p')
-        teams = "%s @ %s" % (game['teams']['away']['team']['teamName'],
-                             game['teams']['home']['team']['teamName'])
-        broadcasts = []
-        for stations in game['broadcasts']:
-            broadcasts.append(stations['name'])
-        schedule.append("%s%s ET (%s)" % (teams.ljust(25),
-                                          game_date.rjust(8),
-                                          ', '.join(broadcasts)))
+    """Return the schedule for either the current or specified day or for a specified team"""
+    j = None
+    if inp is None:
+        # return schedule for current day
+        inp = "today"
+
+    inp = inp.lower()
+    # Check for a team name/nickname in the input
+    for team in NHL_TEAMS:
+        for teamname in team[1]:
+            if inp == teamname:
+                # Return the team schedule for one week after today
+                j = get_nhl_json(get_date('today'),
+                                 end_date=get_date('next week'),
+                                 team_id=team[0])
+    if j is not None:
+        # We want the team schedule
+        schedule = []
+        for date in j['dates']:
+            # this is assuming only one game per day...
+            game = date['games'][0]
+            teams = "%s @ %s" % (game['teams']['away']['team']['teamName'],
+                                 game['teams']['home']['team']['teamName'])
+            broadcasts = []
+            for stations in game['broadcasts']:
+                broadcasts.append(stations['name'])
+            game_date = parse(game['gameDate']).astimezone(EASTERN).strftime('%-I:%M %p')
+            schedule.append("%s%s ET (%s)" % (teams.ljust(25),
+                                              game_date.rjust(8),
+                                              ', '.join(broadcasts)))
+        return schedule
+    else:
+        # We want the schedule of today or some other day
+        date = get_date(inp)
+        j = get_nhl_json(date)
+        schedule = []
+        for game in j['dates'][0]['games']:
+            game_date = parse(game['gameDate']).astimezone(EASTERN).strftime('%-I:%M %p')
+            teams = "%s @ %s" % (game['teams']['away']['team']['teamName'],
+                                 game['teams']['home']['team']['teamName'])
+            broadcasts = []
+            for stations in game['broadcasts']:
+                broadcasts.append(stations['name'])
+            schedule.append("%s%s ET (%s)" % (teams.ljust(25),
+                                              game_date.rjust(8),
+                                              ', '.join(broadcasts)))
     return schedule
 
 @hook.command('scores', autohelp=False)
---
+++ plugins/nhlutil.py
@@ -0,0 +1,63 @@
+import parsedatetime as pdt
+
+from util import http
+
+CAL = pdt.Calendar()
+
+NHL_TEAMS = [
+    (1, ('njd', 'nj', 'devils')),
+    (2, ('nyi', 'islanders')),
+    (3, ('nyr', 'rangers', 'rags')),
+    (4, ('phi', 'philly', 'flyers')),
+    (5, ('pit', 'pittsburgh', 'pens', 'penguins')),
+    (6, ('bos', 'boston', 'bruins')),
+    (7, ('buf', 'buffalo', 'sabres')),
+    (8, ('mon', 'montreal', 'canadiens', 'habs')),
+    (9, ('ott', 'ottawa', 'senators', 'sens')),
+    (10, ('tor', 'toronto', 'leafs', 'maple leafs')),
+    (12, ('car', 'carolina', 'hurricanes', 'canes')),
+    (13, ('fla', 'florida', 'panthers', 'cats')),
+    (14, ('tbl', 'tampa', 'tampa bay', 'lightning', 'bolts')),
+    (15, ('was', 'washington', 'capitals', 'caps')),
+    (16, ('chi', 'chicago', 'blackhawks', 'hawks')),
+    (17, ('det', 'detroit', 'red wings', 'wings', 'wangs')),
+    (18, ('nsh', 'nashville', 'predators', 'preds')),
+    (19, ('stl', 'st. louis', 'st louis', 'blues')),
+    (20, ('cgy', 'calgary', 'flames')),
+    (21, ('col', 'colorado', 'avalanche', 'avs')),
+    (22, ('edm', 'edmonton', 'oilers', 'oil')),
+    (23, ('van', 'vancouver', 'canucks')),
+    (24, ('ana', 'anaheim', 'ducks', 'dorks')),
+    (25, ('dal', 'dallas', 'stars')),
+    (26, ('lak', 'la', 'los angeles', 'kings')),
+    (28, ('sjs', 'sj', 'san jose', 'sharks', 'teal boys')),
+    (29, ('cbj', 'cjb', 'lumbus', 'columbus', 'blue jackets', 'bjs')),
+    (30, ('min', 'minnesota', 'wild')),
+    (52, ('wpg', 'winnipeg', 'jets')),
+    (53, ('ari', 'arizona', 'coyotes', 'yotes'))
+]
+
+def get_scores(inp, status):
+    """Return NHL games with a specific status"""
+    j = get_nhl_json(get_date('today'))
+    scores = []
+    games = j['dates'][0]['games']
+    for game in games:
+        if game['status']['abstractGameState'] == status:
+            scores.append(game)
+    return scores
+
+def get_date(inp):
+    """Parse the input as a date and return a format suitable for the nhl.com API"""
+    if not inp:
+        inp = 'today'
+    return CAL.parseDT(inp)[0].date().isoformat()
+
+def get_nhl_json(start_date, end_date=None, team_id=None):
+    nhl_schedule_url = "https://statsapi.web.nhl.com/api/v1/schedule?startDate=%s&endDate=%s&expand=schedule.teams,schedule.broadcasts.all,schedule.linescore"
+    if end_date is None:
+        end_date = start_date
+    if team_id:
+        return http.get_json(nhl_schedule_url % (start_date, end_date) + "&teamId=%s" % team_id)
+    else:
+        return http.get_json(nhl_schedule_url % (start_date, end_date))
